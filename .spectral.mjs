2eec46ccf76c7df4b9112f76ecbe757c72c783bf
Onosemuode Ikuesiri Kola <ikuesiri.onosemuode@gmail.com>
2023-06-27 12:44:19 +0100

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODAzOQ/spectral.js?branch_slug=main&token=4c7ad6ec-092a-46c3-ba8c-ecdaa0f66f7c";
+export default { extends: ruleset };
